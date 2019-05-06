64aae0d7e9cf5ab012f1b47a695683b6b317e7b2
skarakulak <serkankarakulak@outlook.com>
2019-05-06 19:15:19 -0400

    cleaned redunds

--- train11.py
+++ train11.py
@@ -11,13 +11,11 @@ parser.add_argument('-w','--weights-version-load', type=str, default='v0', help=
 parser.add_argument('-x','--weights-version-save', type=str, default='v0', help='version of the model weight checkpoint to load')
 parser.add_argument('-o','--set-optimizer', type=str, default='adam', help="optimizer ('adam' or 'sgd')")
 parser.add_argument('-l','--lr', type=float, default=0.01, help='learning rate')
-parser.add_argument('-c','--coef-unsup-cdist-loss', type=float, default=0.01, help='multiplier for loss induces from cluster distances')
-parser.add_argument('-p','--print-freq', type=int, default=100, help='multiplier for the variance loss')
-parser.add_argument('-n','--num-of-workers', type=int, default=3, help='number of workers for the dataprep')
-parser.add_argument('-C','--hpc', type=str, default='prince', help='prince/cassio/bigpurple')
+parser.add_argument('-p','--print-freq', type=int, default=2000, help='multiplier for the variance loss')
+parser.add_argument('-n','--num-of-workers', type=int, default=4, help='number of workers for the dataprep')
+parser.add_argument('-C','--hpc', type=str, default='cassio', help='prince/cassio/bigpurple')
 parser.add_argument('-u','--user', type=str, default='sk7685', help='hpc username to be used when determining paths')
 parser.add_argument('-N','--num-of-clusters', type=int, default=2000, help='number of clusters')
-parser.add_argument('-g','--loss-g-multiplier', type=float, default=1.0, help='multiplier of generator loss for the resnet model')
 parser.add_argument('-f','--fake-cse-multiplier', type=float, default=0.0, help='multiplier of cse loss for the fake examples')
 parser.add_argument('-k','--kld-multiplier', type=float, default=1.0, help='multiplier for the kl-div')
 
@@ -26,5 +24,4 @@ args = parser.parse_args()
 print('\nversion name: ' + args.version +'\n')
 
 train_and_val(args)
-
 
