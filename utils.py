6fec3e906c80d3fd67ce3718adb0cf5ca85fde63
skarakulak <serkankarakulak@outlook.com>
2019-05-07 00:49:08 -0400

    cleanup

--- utils.py
+++ utils.py
@@ -35,7 +35,6 @@ def write_to_log(log_path,str_to_log):
 def image_loader(path, batch_size, num_workers=3, pin_memory = True, valid_crop = 84):
     transform_train = transforms.Compose(
         [   
-            transforms.ColorJitter(hue=.05, saturation=.05),
             transforms.RandomRotation(12, resample=PIL.Image.BILINEAR),
             transforms.RandomResizedCrop(84),
             transforms.RandomHorizontalFlip(),
